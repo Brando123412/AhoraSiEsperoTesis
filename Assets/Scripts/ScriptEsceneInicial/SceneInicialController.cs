693a9fed5d7eafa36153add0d4dd2f9c969eb80a
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2024-10-31 14:10:27 -0500

    31/10/2024

--- Assets/Scripts/ScriptEsceneInicial/SceneInicialController.cs
+++ Assets/Scripts/ScriptEsceneInicial/SceneInicialController.cs
@@ -2,14 +2,27 @@ using ScenesManager;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneInicialController : MonoBehaviour
 {
     [SerializeField] int misiones;
-
+    [SerializeField] Toggle[] ArregloObjetivos;
+    private void Awake()
+    {
+       
+    }
     public void GoToGame(SceneConfiguration scene)
     {
         SceneGlobalManager.Instance.LoadScene(scene);
     }
-
+    public void asd()
+    {
+        ArregloObjetivos[1].isOn = true;
+    }
+    public void CheckWork(int i){
+        ArregloObjetivos[i].isOn = true;
+    }
 }
+
+
